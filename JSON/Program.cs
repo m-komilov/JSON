c94f096263cddd8c3084084cf8f7d272d3320997
m-komilov <muhammadabdulloh.uzinfocom@gmail.com>
2023-05-30 20:21:31 +0500

    Add project files.

---
+++ JSON/Program.cs
@@ -0,0 +1,40 @@
+﻿using JSON;
+using Newtonsoft.Json;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       string url = "https://my-json-server.typicode.com/typicode/demo/posts";
+
+        HttpClient httpClient = new HttpClient();
+
+        try
+        {
+            var httpResponseMessage = httpClient.GetAsync(url).Result;
+
+            string jsonResponce = httpResponseMessage.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(jsonResponce);
+
+            var myPosts = JsonConvert.DeserializeObject<Post[]>(jsonResponce);
+
+            //Console.WriteLine(myPosts);
+
+            foreach (var post in myPosts)
+            {
+                Console.WriteLine($" ID: {post.Id},  Title: {post.Title} ");
+            }
+
+        } 
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            httpClient.Dispose();
+        }
+
+    }
+}
